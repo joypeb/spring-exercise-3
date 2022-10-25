d87516c9934d16efafd8d3085ba71161e93983a2
박은빈 <joypeb@naver.com>
2022-10-25 11:35:44 +0900

    에러 핸들링

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -17,20 +17,66 @@ public class UserDao {
     }
 
 
-    public void add(final User user) throws ClassNotFoundException, SQLException {
+    public void add(final User user) {
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        Connection c = connectionMaker.makeConnection();
+        try {
+            c = connectionMaker.makeConnection();
 
-        PreparedStatement ps = c.prepareStatement(
-                "insert into likelionDB.users(id, name, password) values(?, ?, ?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+            ps = c.prepareStatement(
+                    "insert into likelionDB.users(id, name, password) values(?, ?, ?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        ps.executeUpdate();
+            ps.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
 
-        ps.close();
-        c.close();
+    public void deleteAll() {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = connectionMaker.makeConnection();
+            ps = c.prepareStatement("delete from likelionDB.users");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 
     public User findById(String id) {
@@ -39,30 +85,46 @@ public class UserDao {
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        try{
+        try {
             c = connectionMaker.makeConnection();
             ps = c.prepareStatement("select * from likelionDB.users where id = ?");
-            ps.setString(1,id);
+            ps.setString(1, id);
 
             rs = ps.executeQuery();
 
             User user = null;
 
-            if(rs.next()) {
-                user = new User(rs.getString(1),rs.getString(2),rs.getString(3));
+            if (rs.next()) {
+                user = new User(rs.getString(1), rs.getString(2), rs.getString(3));
             }
 
             if (user == null) throw new EmptyResultDataAccessException(1);
-
-            rs.close();
-            ps.close();
-            c.close();
 
             return user;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
 
 
